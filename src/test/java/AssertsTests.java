75654186e74ce30eb7db5cf2c9a50aca81607671
TechnoStudy <info@techno.study>
2020-03-07 15:41:56 -0500

    assert equals no order

--- src/test/java/AssertsTests.java
+++ src/test/java/AssertsTests.java
@@ -1,8 +1,12 @@
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class AssertsTests {
     @Test
     public void testArrays(){
-        
+        Object[] firstArray = new Object[]{ 1,2,3,4,5,6,7,8,9,10 };
+        Object[] secondArray = new Object[]{ 1,3,2,4,5,6,7,8,9,10 };
+        Assert.assertEqualsNoOrder( firstArray, secondArray );
+
     }
 }
