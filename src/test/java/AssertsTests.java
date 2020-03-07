f25cf031e832139c1d0f95c7bf2ed1e76b462297
TechnoStudy <info@techno.study>
2020-03-07 16:10:37 -0500

    using hamcrest

--- src/test/java/AssertsTests.java
+++ src/test/java/AssertsTests.java
@@ -1,6 +1,12 @@
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.Arrays;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.Matchers.containsInAnyOrder;
+
 public class AssertsTests {
     @Test
     public void testArrays(){
@@ -8,5 +14,6 @@ public class AssertsTests {
         Object[] secondArray = new Object[]{ 1,3,2,4,5,6,7,8,9,10 };
         Assert.assertEqualsNoOrder( firstArray, secondArray );
 
+        assertThat( Arrays.asList(firstArray), containsInAnyOrder( secondArray ));
     }
 }
